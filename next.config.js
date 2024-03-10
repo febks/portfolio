6a282e527decc7c64281359369e0b55594a9821e
Febriana Kusumah <kusumahfebriana@gmail.com>
2024-03-11 00:07:44 +0700

    fix config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: {
+    target: 'static'
+  },
   distDir: 'dist',
+  path: '/app',
   images: {
     unoptimized: true
   }
